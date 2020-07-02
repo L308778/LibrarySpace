cfbb3de8dced7f602e5c5292c4bc7bd8ca5d5e69
L308778 <50208337+L308778@users.noreply.github.com>
2020-07-02 22:51:54 +0200

    Added style abstraction. The style file is in the assets folder under styles.js

---
+++ assets/styles.js
@@ -0,0 +1,82 @@
+import {StyleSheet} from "react-native"
+
+
+export const background = {
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor:"white"
+      },
+}
+
+export const header = {
+    text: {
+        padding: 20,
+        color: 'darkred',
+        alignSelf: 'center',
+        marginVertical: 10,
+        fontSize: 50,
+        fontWeight: 'bold',
+      },
+}
+
+export const button = {
+    text: {
+        marginRight: 40,
+        marginLeft: 40,
+        fontWeight: 'bold',
+        fontSize: 30,
+        color: 'white',
+      },
+    touch: {
+        paddingTop: 20,
+        paddingBottom: 20,
+        textAlign: 'center',
+        backgroundColor: 'darkred',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'white',
+        alignContent: 'center',
+        margin: 20,
+      },
+}
+
+export const flatlistscreen = {
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    touch: {
+        paddingTop: 20,
+        paddingBottom: 20,
+        alignContent: 'center',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        borderWidth: 5,
+        borderColor: 'darkred',
+        margin: 10,
+      },
+      text: {
+        fontSize: 20,
+        color: 'darkred',
+        fontWeight:"bold",
+        alignSelf: 'center',
+      },
+}
+
+export const introscreen = {
+    view: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white'
+      },
+      Icon: {
+        alignSelf: 'center',
+      }
+}
+
+
+
+
+
